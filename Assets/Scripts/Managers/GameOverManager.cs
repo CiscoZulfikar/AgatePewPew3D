37a9d924e77cab25a4045b4126fded9ea3c65c0e
CiscoZ073 <13518073@std.stei.itb.ac.id>
2021-02-15 21:53:29 +0700

    added ALL

---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    public UnityEngine.UI.Text warningText;
+    public PlayerHealth playerHealth;
+
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+        }
+    }
+
+    public void ShowWarning(float enemyDistance)
+    {
+        warningText.text = string.Format("! {0} m", Mathf.RoundToInt(enemyDistance));
+        anim.SetTrigger("Warning");
+    }
+}
